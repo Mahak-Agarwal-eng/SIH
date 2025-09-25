fd83729e538f87065fce3aedd41a155838c52d4e
suhanee-gupta <suhaneegupta2905@gmail.com>
2025-09-26 01:59:03 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const certificateRoutes = require('./routes/certificate.routes');
+const adminRoutes = require('./routes/admin.routes');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/certificates', certificateRoutes);
+app.use('/api/admin', adminRoutes);
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI, {
